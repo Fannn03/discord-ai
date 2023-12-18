686415b6c83934c4646d3788dbd69e3d85f9a701
Fannn03 <fann.fann771@gmail.com>
2023-12-18 22:42:56 +0700

    add open ai

--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -1,10 +1,33 @@
 import { Message } from "discord.js";
+import { RateLimitError } from "openai";
+import { openAI } from "configs/openai";
 import { eventInterface } from "domains/models/event";
+import { MessageError } from "common/adapters/errors/message-error";
 
 export default {
   name: 'messageCreate',
   once: false,
-  execute: async function (message: Message): Promise<void> {
-   if(message.author.bot) return;
+  execute: async function (message: Message): Promise<void | Message | MessageError> {
+    if(message.author.bot || message.channelId !== "1186307613231689808") return;
+    if(!message.content.length || message.content == " ") return new MessageError(message, "Message prompt cannot has empty or blank value!");
+
+    try {
+      await message.channel.sendTyping();
+
+      const completion = await openAI.chat.completions.create({
+        model: 'gpt-3.5-turbo-16k-0613',
+        messages: [
+          {role: "user", content: message.content}
+        ]
+      })
+    
+      return await message.reply(completion.choices[0].message.content as string);
+    } catch (err: any) {
+      if(err instanceof RateLimitError) {
+        new MessageError(message, "Rate limit, try again later.");
+      } else {
+        new MessageError(message, err.message)
+      }
+    }
   }
 } as eventInterface
