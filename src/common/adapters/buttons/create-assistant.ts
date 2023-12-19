2f8aa4c178411d53781fbc02e6c9dd797d0b4484
Fannn03 <fann.fann771@gmail.com>
2023-12-19 18:21:02 +0700

    add create assistant modal

---
+++ src/common/adapters/buttons/create-assistant.ts
@@ -0,0 +1,30 @@
+import { ActionRowBuilder, ButtonInteraction, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+
+export default {
+  name: 'createAssistant',
+  execute: async function (interaction: ButtonInteraction) {
+    const modal = new ModalBuilder()
+      .setCustomId('createAssistant')
+      .setTitle("Create Assistant")
+
+    const setName = new TextInputBuilder()
+      .setCustomId('name')
+      .setLabel("Assistant Name")
+      .setPlaceholder("English tutor")
+      .setMinLength(4)
+      .setStyle(TextInputStyle.Short)
+
+    const setInstructions = new TextInputBuilder()
+      .setCustomId('intructions')
+      .setLabel("Intructions")
+      .setPlaceholder("You are a personal english tutor. Teach me how to speak english well")
+      .setMinLength(4)
+      .setStyle(TextInputStyle.Paragraph)
+
+    const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(setName);
+    const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(setInstructions);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+    return await interaction.showModal(modal);
+  }
+}
